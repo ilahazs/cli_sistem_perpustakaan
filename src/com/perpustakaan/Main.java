ff99960e3094a2f74f360b37699729f89574a675
ilahazs <ilahazs.weebs@gmail.com>
2021-10-31 00:42:21 +0700

    initial commit

---
+++ src/com/perpustakaan/Main.java
@@ -0,0 +1,91 @@
+package com.perpustakaan;
+
+class Siswa {
+   String nama, kelas, jurusan, NIS;
+   int usia;
+   // boolean status = false;
+   KeAnggotan anggota;
+
+   Siswa(String nama, String kelas, String jurusan, String NIS) {
+      this.nama = nama;
+      this.kelas = kelas;
+      this.jurusan = jurusan;
+      this.NIS = NIS;
+   }
+
+   void profile() {
+      // String statusAnggota = (this.status == true ? "terdaftar" : "blm terdaftar");
+      System.out.println("\nPROFILE\n========================\n");
+      System.out.printf("Nama       : %s\n", this.nama);
+      System.out.printf("Kelas      : %s\n", this.kelas);
+      System.out.printf("Jurusan    : %s\n", this.jurusan);
+      System.out.printf("NIS        : %s\n", this.NIS);
+      System.out.printf("Anggota    : %s\n", this.anggota.getStatus());
+      System.out.println("\n========================");
+   }
+
+   void daftarAnggota(KeAnggotan anggota) {
+      this.anggota = anggota;
+      this.anggota.status = true;
+   }
+
+}
+
+// Class lain
+class KeAnggotan {
+   int UID;
+   boolean status = false;
+   String nama, NIS;
+
+   KeAnggotan(Siswa siswa) {
+      this.nama = siswa.nama;
+      this.UID = 1;
+      this.NIS = siswa.NIS;
+   }
+
+   boolean getStatus() {
+      return this.status;
+   }
+
+   void pinjamBuku(Buku buku) {
+      buku.stock -= 1;
+   }
+}
+
+class Buku {
+   String judul, penulis;
+   int jumlah_halaman, stock;
+
+   Buku(String judul, String penulis, int jumlah_halaman, int stock) {
+      this.judul = judul;
+      this.penulis = penulis;
+      this.jumlah_halaman = jumlah_halaman;
+      this.stock = stock;
+   }
+
+   void show() {
+      // String statusAnggota = (this.status == true ? "terdaftar" : "blm terdaftar");
+      System.out.println("\nDETAIL BUKU\n========================\n");
+      System.out.printf("Judul    : %s\n", this.judul);
+      System.out.printf("Penulis  : %s\n", this.penulis);
+      System.out.printf("Hal      : %s\n", this.jumlah_halaman + " hal");
+      System.out.printf("Stock    : %s\n", this.stock + " item");
+      System.out.println("\n========================");
+   }
+
+}
+
+public class Main {
+   public static void main(String[] args) {
+      Siswa siswa1 = new Siswa("Ilahazs", "XII", "RPL", "1920118111");
+      KeAnggotan anggota1 = new KeAnggotan(siswa1);
+      siswa1.daftarAnggota(anggota1);
+      // siswa1.profile();
+
+      // EKSEKUSI BUKU
+      Buku buku1 = new Buku("Ksatria Batman", "Diluc", 25, 10);
+      buku1.show();
+      anggota1.pinjamBuku(buku1);
+      buku1.show();
+   }
+}
